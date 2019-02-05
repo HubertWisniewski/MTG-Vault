be750aec6487debc63c9801aa88ebb1477585cd9
Hubert Wiśniewski <hubert_w@autograf.pl>
2019-02-05 01:53:42 +0100

    Trying to make a popup

--- src/components/CardSearcher/CardSearcher.js
+++ src/components/CardSearcher/CardSearcher.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardSearcherForm from "../CardSearcherForm//CardSearcherForm";
 import { Link } from "react-router-dom";
 import firebase from 'firebase'
+import { Popup } from 'semantic-ui-react'
 
 class CardSearcher extends Component {
   state = {
@@ -13,8 +14,8 @@ class CardSearcher extends Component {
 
   clearList = clear => this.setState({ results: clear });
 
-  processSearchPhrase = (cardName, cardId, setName) =>
-    this.state.results.push({ name: cardName, id: cardId, set: setName });
+  processSearchPhrase = (cardName, cardId, setName, imgUrl) =>
+    this.state.results.push({ name: cardName, id: cardId, set: setName, img: imgUrl });
 
   addToCollection = result => {
     firebase.database().ref('users/' + this.props.user.uid + '/collection').push(result)
@@ -36,7 +37,11 @@ class CardSearcher extends Component {
             this.state.results.map(result => (
               <li key={result.id}>
                 {" "}
-                <Link to={`/card/${result.id}`}> {result.name} </Link>{" "}
+                <Link to={`/card/${result.id}`}>
+
+                 {result.name} 
+                 
+                 </Link>{" "}
                 {result.set}
             <button onClick={() => this.addToCollection(result)}>Add</button>
               </li>
